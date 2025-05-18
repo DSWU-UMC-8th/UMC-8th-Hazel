4f3b1cff0e144460a010e352d5f9397bf8580383
hmj6589 <hmj6589@gmail.com>
2025-05-18 20:01:55 +0900

    [FIX] CategoriesExistValidator 수정

--- src/main/java/umc/study/validation/validator/CategoriesExistValidator.java
+++ src/main/java/umc/study/validation/validator/CategoriesExistValidator.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.study.apiPayload.code.status.ErrorStatus;
 import umc.study.repository.FoodCategoryRepository;
+import umc.study.service.FoodCategoryService;
 import umc.study.validation.annotation.ExistCategories;
 
 import java.util.List;
@@ -14,7 +15,8 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
 
-    private final FoodCategoryRepository foodCategoryRepository;
+    //private final FoodCategoryRepository foodCategoryRepository;
+    private final FoodCategoryService foodCategoryService;
 
     @Override
     public void initialize(ExistCategories constraintAnnotation) {
@@ -23,8 +25,10 @@ public class CategoriesExistValidator implements ConstraintValidator<ExistCatego
 
     @Override
     public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
-        boolean isValid = values.stream()
-                .allMatch(value -> foodCategoryRepository.existsById(value));
+        //boolean isValid = values.stream()
+        //        .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        boolean isValid = foodCategoryService.existsByIds(values);
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
